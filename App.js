8a5fde0991cfb72bcff7cff8ba813695c2f7696f
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2024-04-02 14:23:30 +0530

    rendering repo list

--- App.js
+++ App.js
@@ -1,20 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from "./src/components/Main";
+import RepositoryList from "./src/components/RepositoryList";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hellow</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Main />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
